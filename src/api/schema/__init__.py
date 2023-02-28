cc314684c711c5e879683e82159a4d342f9a90d1
Ada Pedersen <github@adapdr.me>
2023-02-28 15:15:57 +0100

    schema loader now works

--- src/api/schema/__init__.py
+++ src/api/schema/__init__.py
@@ -9,12 +9,13 @@ APISchema: dict = {}
 for schema in Path(AutoLoader.schema_location).iterdir():
     if "__" in str(schema):
         continue
-
     name = schema.stem
-    module = f"{AutoLoader.schema_location}/{name}".replace('/', '.')
+    module_name = f"{AutoLoader.schema_location}/{name}".replace('/', '.')
+    schema_container = getattr(
+        import_module(module_name),
+        f"{name.capitalize()}Schema"
+    )
+
     APISchema.update({
-        name: getattr(
-            import_module(module),
-            f"{name.capitalize()}Schema"
-        )
+        name: schema_container
     })
