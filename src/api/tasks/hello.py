f06e2c3ce13dce7f3de2137ffbcca498baf6fa8d
Ada Pedersen <github@adapdr.me>
2023-02-28 15:18:25 +0100

    updated tasks loader

--- src/api/tasks/hello.py
+++ src/api/tasks/hello.py
@@ -1,9 +1,10 @@
 """File contains the HelloTasks container"""
+from api.schema.hello import HelloSchema
 
 
 class HelloTasks:
     """Tasks container for the HelloRouter"""
 
     @staticmethod
-    def create_later(entity: object):
+    async def do_after(entity: HelloSchema):
         print(f'hello {entity.name}')
