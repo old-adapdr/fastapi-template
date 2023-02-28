939526df6c7220ebff10acf40705309fb112cb66
Ada Pedersen <github@adapdr.me>
2023-02-28 15:17:31 +0100

    updated schema

--- src/api/schema/hello.py
+++ src/api/schema/hello.py
@@ -12,8 +12,28 @@ class Hello(BaseModel):
     Model for a `Hello` object
     """
 
-    uid: UUID = Field(uuid4(), description="Unique IDentifier", alias="uid")
-    name: str = Field(..., description="Who to say hello to")
+    __uuid__: UUID = Field(description="Unique IDentifier", default_factory=uuid4)
+
+    name: str = Field(None, description="Who to say hello to")
+    pronouns: str = Field('Champion', description="Pronoun to use when saying hello")
+
+    __created_at__: str = Field(..., description="When the record was created")
+    __updated_at__: str = Field(..., description="When the record was last updated")
+    __deleted_at__: str = Field(..., description="When the record was deleted")
+
+    class Config:
+        orm_mode = True
+
+
+class HelloList(BaseModel):
+    """
+    Model for a `Hello` object
+    """
+
+    data: List[Hello]
+
+    class Config:
+        orm_mode = False
 
 
 class HelloSchema:
